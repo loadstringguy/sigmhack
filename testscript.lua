2740cddb79f81f2dbb48a27d01a9d57c13802e6e
woops.cc <140357378+loadstringguy@users.noreply.github.com>
2024-11-01 16:25:00 -0600

    Update testscript.lua

--- testscript.lua
+++ testscript.lua
@@ -1,21 +1,20 @@
-if getgenv().sigmhack then warn("Script already loaded or is loading") return end
+if getgenv().sigmhack then
+    warn("Script already loaded or is loading")
+    return
+end
 getgenv().sigmhack = true
-
 
 print('Loading AC Bypass!')
 if not LPH_OBFUSCATED then
     getfenv().LPH_NO_VIRTUALIZE = function(f) return f end
-  end
-  
+end
 
-  local ReplicatedStorage = game:GetService("ReplicatedStorage")
-  
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Handshake = ReplicatedStorage.Remotes.CharacterSoundEvent
+local Hooks = {}
+local HandshakeInts = {}
 
-  local Handshake = ReplicatedStorage.Remotes.CharacterSoundEvent
-  local Hooks = {}
-  local HandshakeInts = {}
-  
-  LPH_NO_VIRTUALIZE(function()
+LPH_NO_VIRTUALIZE(function()
     for i, v in getgc() do
         if typeof(v) == "function" and islclosure(v) then
             if (#getprotos(v) == 1) and table.find(getconstants(getproto(v, 1)), 4000001) then
@@ -23,12 +22,12 @@ if not LPH_OBFUSCATED then
             end
         end
     end
-  end)()
-  
-  Hooks.__namecall = hookmetamethod(game, "__namecall", LPH_NO_VIRTUALIZE(function(self, ...)
+end)()
+
+Hooks.__namecall = hookmetamethod(game, "__namecall", LPH_NO_VIRTUALIZE(function(self, ...)
     local Method = getnamecallmethod()
     local Args = {...}
-  
+
     if not checkcaller() and (self == Handshake) and (Method == "fireServer") and (string.find(Args[1], "AC")) then
         if (#HandshakeInts == 0) then
             HandshakeInts = {table.unpack(Args[2], 2, 18)}
@@ -38,14 +37,12 @@ if not LPH_OBFUSCATED then
             end
         end
     end
-  
+
     return Hooks.__namecall(self, ...)
-  end))
-  
-  task.wait(1)
+end))
+
+task.wait(1)
 print('Success! Now Loading..')
-
-
 
 local debris = game:GetService("Debris")
 local contentProvider = game:GetService("ContentProvider")
@@ -64,29 +61,21 @@ local mouse = player:GetMouse()
 local camera = workspace.CurrentCamera
 local values = replicatedStorage:FindFirstChild("Values")
 
-local Library = https://raw.githubusercontent.com/loadstringguy/sigmhack/refs/heads/main/library.lua
-local example = library:CreateWindow({
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/loadstringguy/sigmhack/refs/heads/main/library.lua"))()
+local example = Library:CreateWindow({
     text = "sigmhack : main"
 })
+
 example:AddToggle("Magnets", function(state)
-getfenv().mags =(state and true or false)
-Workspace = game:GetService("Workspace")
+    getfenv().mags = (state and true or false)
+    local Workspace = game:GetService("Workspace")
 
-local rangee = 25
+    local distance = 25
 
-Workspace.ChildAdded:Connect(function(Child)
-
-if Child:IsA("BasePart") and Child.Name == "Football" then
-
-Child.Size = Vector3.new(rangee, rangee, rangee)
-
-Child.CanCollide = false
-
-end
-
-local example = library:CreateWindow({
-        text = "Physics"
-})
-
-
+    Workspace.ChildAdded:Connect(function(Child)
+        if Child:IsA("BasePart") and Child.Name == "Football" then
+            Child.Size = Vector3.new(distance, distance, distance)
+            Child.CanCollide = false
+        end
+    end)
 end)
